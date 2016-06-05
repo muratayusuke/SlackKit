678e9073b604b3a205cb43ce7e013467bb749aab
muratayusuke <info@muratayusuke.com>
2016-06-06 01:48:45 +0900

    Add rx_history to channel

---
+++ SlackKit/Sources/Channel+Rx.swift
@@ -0,0 +1,28 @@
+//
+//  Channel+Rx.swift
+//  SlackKit
+//
+//  Created by 佑介 村田 on 2016/06/05.
+//  Copyright © 2016年 Launch Software LLC. All rights reserved.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+extension Channel {
+
+    public func rx_history(latest: String = "\(NSDate().timeIntervalSince1970)", oldest: String = "0", inclusive: Bool = false, count: Int = 100, unreads: Bool = false) -> Observable<History> {
+        guard let id = id, client = client else { return Observable.never() }
+        if isMPIM == true {
+            return client.webAPI.rx_mpimHistory(id, latest: latest, oldest: oldest, inclusive: inclusive, count: count, unreads: unreads)
+        } else if isIM == true {
+            return client.webAPI.rx_imHistory(id, latest: latest, oldest: oldest, inclusive: inclusive, count: count, unreads: unreads)
+        } else if isGroup == true {
+            return client.webAPI.rx_groupHistory(id, latest: latest, oldest: oldest, inclusive: inclusive, count: count, unreads: unreads)
+        } else {
+            return client.webAPI.rx_channelHistory(id, latest: latest, oldest: oldest, inclusive: inclusive, count: count, unreads: unreads)
+        }
+    }
+
+}
--- SlackKit/Sources/SlackWebAPI+Rx.swift
+++ SlackKit/Sources/SlackWebAPI+Rx.swift
@@ -12,11 +12,56 @@ import RxSwift
 
 extension SlackWebAPI {
 
+    //MARK: - Channels
+    public func rx_channelHistory(id: String, latest: String = "\(NSDate().timeIntervalSince1970)", oldest: String = "0", inclusive: Bool = false, count: Int = 100, unreads: Bool = false) -> Observable<History> {
+        guard let request = channelHistoryRequest(id, latest: latest, oldest: oldest, inclusive: inclusive, count: count, unreads: unreads) else {
+            return Observable.empty()
+        }
+        return NSURLSession.sharedSession().rx_JSON(request)
+            .map { response in
+                return History(history: response as? [String: AnyObject])
+            }
+    }
+
+    //MARK: - Messaging
     public func rx_sendMessage(channel: String, text: String, username: String? = nil, asUser: Bool? = nil, parse: ParseMode? = nil, linkNames: Bool? = nil, attachments: [Attachment?]? = nil, unfurlLinks: Bool? = nil, unfurlMedia: Bool? = nil, iconURL: String? = nil, iconEmoji: String? = nil) -> Observable<AnyObject> {
         guard let request = sendMessageRequest(channel, text: text, username: username, asUser: asUser, parse: parse, linkNames: linkNames, attachments: attachments, unfurlLinks: unfurlLinks, unfurlMedia: unfurlMedia, iconURL: iconURL, iconEmoji: iconEmoji) else {
             return Observable.empty()
         }
         return NSURLSession.sharedSession().rx_JSON(request)
+    }
+
+    //MARK: - Groups
+    public func rx_groupHistory(id: String, latest: String = "\(NSDate().timeIntervalSince1970)", oldest: String = "0", inclusive: Bool = false, count: Int = 100, unreads: Bool = false) -> Observable<History> {
+        guard let request = groupHistoryRequest(id, latest: latest, oldest: oldest, inclusive: inclusive, count: count, unreads: unreads) else {
+            return Observable.empty()
+        }
+        return NSURLSession.sharedSession().rx_JSON(request)
+            .map { response in
+                return History(history: response as? [String: AnyObject])
+            }
+    }
+
+    //MARK: - IM
+    public func rx_imHistory(id: String, latest: String = "\(NSDate().timeIntervalSince1970)", oldest: String = "0", inclusive: Bool = false, count: Int = 100, unreads: Bool = false) -> Observable<History> {
+        guard let request = imHistoryRequest(id, latest: latest, oldest: oldest, inclusive: inclusive, count: count, unreads: unreads) else {
+            return Observable.empty()
+        }
+        return NSURLSession.sharedSession().rx_JSON(request)
+            .map { response in
+                return History(history: response as? [String: AnyObject])
+            }
+    }
+
+    //MARK: - MPIM
+    public func rx_mpimHistory(id: String, latest: String = "\(NSDate().timeIntervalSince1970)", oldest: String = "0", inclusive: Bool = false, count: Int = 100, unreads: Bool = false) -> Observable<History> {
+        guard let request = mpimHistoryRequest(id, latest: latest, oldest: oldest, inclusive: inclusive, count: count, unreads: unreads) else {
+            return Observable.empty()
+        }
+        return NSURLSession.sharedSession().rx_JSON(request)
+            .map { response in
+                return History(history: response as? [String: AnyObject])
+            }
     }
 
 }
